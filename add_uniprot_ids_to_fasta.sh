a040c92ec7e2f90d271ebbb5e331190ca53e6b08
Joe Colgan <t.colgan@qmul.ac.uk>
2017-04-08 19:23:38 +0100

    Update add_uniprot_ids_to_fasta.sh

--- add_uniprot_ids_to_fasta.sh
+++ add_uniprot_ids_to_fasta.sh
@@ -17,12 +17,60 @@
 ## were subsetted. 
 
 # BLAST command
-#blastp -query ../Drosophila_UniProt.fasta \
-#       -db 2017-04-07_Bter_2015_v1.fasta \
+#blastp -query 2017-04-07_Bter_2015_v1.fasta \
+#       -db ../Apis_Uniprot \
 #       -evalue 1e-06 \
 #       -num_threads 10 \
 #       -outfmt 6 | sort -k1,1 -k12,12nr -k11,11n | sort -u -k1,1 --merge \ |
-#       > best_single_hits.bter_vs_dros.blastn
+#       > best_single_hits.bter_vs_apis.blastn
+
+## The results of the BLAST searches indicate that:
+# 9908 Bter proteins annotated from Apis search
+# 8418 Bter proteins annotated from Dros search
+
+## Four stages of the data:
+# 1) Bter matches to both Apis and Dros
+# 2) Bter matches to only Apis
+# 3) Bter matches to only Dros
+# 4) Bter protein sequences without matches to Apis or Dros
+
+## Stage One: Annotate proteins with matches to Apis and Dros
+# For files containing the BLAST outputs, parse the field containing Bter protein IDs.
+# Sort the Ids and take only unique matches and output
+for name in *.blastn; do cut -f 1 "$name" | sort | uniq > "$name".Bter_Ids.txt; done
+
+## Combined Bter_IDs for Apis and Dros BLAST outputs
+cat *Bter_Ids* > Bter_IDs_with_Apis_and_Dros_homologues.txt
+
+## Move to a folder for convenience
+mv Bter_IDs_with_Apis_and_Dros_homologues.txt stage_1_Apis_Dros_matches/
+
+## Sort and count unique 
+sort Bter_IDs_with_Apis_and_Dros_homologues.txt | uniq -c > Bter_IDs_with_Apis_and_Dros_homologues.tmp
+
+sort Bter_IDs_with_Apis_and_Dros_homologues.txt | uniq -c | awk '$1==2' - > Bter_IDs_with_Apis_and_Dros_homologues.confirmed.txt
+wc -l Bter_IDs_with_Apis_and_Dros_homologues.confirmed.txt #6873
+
+awk '{ print $2 }' Bter_IDs_with_Apis_and_Dros_homologues.confirmed.txt > Bter_IDs_with_Apis_and_Dros_homologues.confirmed.tmp
+mv Bter_IDs_with_Apis_and_Dros_homologues.confirmed.tmp Bter_IDs_with_Apis_and_Dros_homologues.confirmed.txt
+
+## Subset the sequence information for homologues
+grep -f Bter_IDs_with_Apis_and_Dros_homologues.confirmed.txt \
+        ../best_single_hits.bter_vs_apis.blastn | \
+        cut -f 1,2 - > Bter_IDs_with_Apis_and_Dros_homologues.confirmed.Apis_Ids.txt 
+
+grep -f Bter_IDs_with_Apis_and_Dros_homologues.confirmed.txt \
+        ../best_single_hits.bter_vs_dros.blastn | \
+        cut -f 1,2 - > Bter_IDs_with_Apis_and_Dros_homologues.confirmed.Dros_Ids.txt 
+
+## Using the subset FASTA names (have identified FASTA sequences that are in Bter, Amel and Dmel)
+paste Bter_IDs_with_Apis_and_Dros_homologues.confirmed.Apis_Ids.txt \
+      Bter_IDs_with_Apis_and_Dros_homologues.confirmed.Dros_Ids.txt | \
+      cut -f 1,2,4 - > Bter_IDs_with_Apis_and_Dros_homologues.confirmed.Bter_Apis_Dros.IDs.txt
+
+## 
+
+sed -i 's,.1|,.1_|,g' Bter_IDs_with_Apis_and_Dros_homologues.confirmed.Bter_Apis_Dros.IDs.uniprot_IDs.tmp
 
 ## Subset the sequence information for homologues
  cut -d '_' -f 1,2 GCF_000214255.1_Bter_1.0_protein.homologue_list.txt > GCF_000214255.1_Bter_1.0_protein.homologue_list.tmp
